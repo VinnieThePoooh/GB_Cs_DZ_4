37a41feb4819207825652c03578ae64dc94e6d5b
Керим Гучигов <forsyte12316gmail.com@MacBook-Air-Kerim.local>
2023-03-01 15:17:56 +0300

    .

---
+++ task25.cs
@@ -0,0 +1,12 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+System.Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Pow(number1, number2);
+System.Console.WriteLine($"{number1} в степени {number2} равно {result}");
---
+++ task27.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+System.Console.WriteLine("Ввведите число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int number = 0;
+while (userNumber>9)
+{
+    number = number + userNumber%10;
+    userNumber = userNumber/10;
+}
+number = number+userNumber;
+System.Console.WriteLine(number);
